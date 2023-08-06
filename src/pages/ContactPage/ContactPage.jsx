ceb09938ba8dcfbe90d9458c822b56a567752ce3
j-vaz12 <jasonvasquezx@gmail.com>
2023-08-06 13:20:59 -0700

    contact css

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,13 +1,29 @@
 import "./ContactPage.css"
+import {Link} from 'react-router-dom';
 
 export default function ContactPage() {
     return (
-        <div className="Contact-Container">
-            <div className="Git-Hub">
-                <h1> Git Hub</h1>
+        <div className="contact-container">
+            <Link className="link" to="https://github.com/j-vaz12"> 
+                <div className="socials">
+                    <h1 className="github"> Git-Hub</h1>
+                    <p> Come chekout my GitHub 🤩 </p>
+                    <img src="" alt="" />
+                </div>
+            </Link>
+
+            <Link className="link" to="https://www.linkedin.com/in/jason-vasquez-codes/"> 
+                <div className="socials">
+                    <h1 className="linkedin"> Linkten</h1>
+                    <p> Lets connect on Liknten 😄</p>
+                    <p>jason-vasquez-codes </p>
+                </div>
+            </Link>
+
+            <div className="socials">
+                <h1> Email</h1>
+                <li> jasonvasquezx@gmail.com </li>
             </div>
-            <h1> Linkten</h1>
-            <h1> Email</h1>
         </div>
     );
 }
