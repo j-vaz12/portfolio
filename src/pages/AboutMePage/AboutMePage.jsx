773976247df8eca7100a735a3e4741b5c62d3d30
j-vaz12 <jasonvasquezx@gmail.com>
2023-08-06 14:25:06 -0700

    about me

--- src/pages/AboutMePage/AboutMePage.jsx
+++ src/pages/AboutMePage/AboutMePage.jsx
@@ -1,13 +1,16 @@
 import "./AboutMePage.css"
+import { Link } from "react-router-dom";
 export default function AboutMe() {
     return(
         <div className="about-container">
             <h1 className="AboutMe-h1">About me</h1>
-            <h3>Web Develper</h3>
+            <h3> Web Develper</h3>
             <p> Hello there! I'm Jason Vasquez, a dedicated problem-solver with a deep-rooted love for programming and a burning desire to carve my path as a developer. My journey into the world of code has been nothing short of exhilarating. From the moment I wrote my first line of code, I knew I had found my calling. The logic, the structure, the elegance of turning complex challenges into elegant solutions—it all resonates with me on a profound level. Transitioning into the role of a developer is not just a career move; it's a natural progression of my inherent curiosity and thirst for knowledge. Every bug I encounter is an opportunity to unravel a mystery, every roadblock a chance to flex my problem-solving muscles. I thrive on the thrill of untangling intricate code and transforming it into a functional masterpiece. My passion for continuous learning fuels my drive. The ever-evolving landscape of technology is a canvas where I eagerly paint with new languages, frameworks, and tools. I relish the chance to embrace the latest trends and adapt them to create innovative and efficient solutions.</p>
             <h1>Skills</h1>
             <p> REACT / JAVASCRIPT / HTML / NODE.JS / CSS / EJS / MONNGOOSE / MOONGODB </p>
             <p> PYTHON / DJANGO / POSTGRESQL / GITHUB / MARKDOWN / AWS S3</p>
+            <h1> Resume</h1>
+            <Link className="resume" to="https://drive.google.com/file/d/1QfcrVMc3sUpy_wgC42VL2_NMmZ-lhgrP/view?usp=sharing"> My Resume </Link>
         </div>
     );
 }
