95a3b63622c782694f4ecaec2f8fb12618bf01dd
j-vaz12 <jasonvasquezx@gmail.com>
2023-08-07 15:22:35 -0700

    project dis

--- src/data/projects.js
+++ src/data/projects.js
@@ -10,30 +10,30 @@ const INFO = {
 		},
 
 		{
-			title: "Poke-Vault",
+			title: "Pokemon-Vault",
 			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+				"Welcome to the Pokémon Vault web application, designed to cater to the needs of Pokémon enthusiasts and beyond. With a user-friendly interface, you can effortlessly search for and add your favorite Pokémon to your personal vault. Whether you're a passionate Pokémon fan or simply curious, the platform invites you to explore and enjoy.",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
 			linkText: "View Project",
-			link: "https://github.com",
+			link: "https://github.com/j-vaz12/pokemon-vault",
 		},
 
 		{
-			title: "Project 3",
+			title: "Rent-A-Car",
 			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+				"Rent-a-Car is a user-friendly and efficient web application designed to simplify the process of renting cars. Whether you're a frequent traveler, a business professional, or someone in need of temporary transportation, Rent-a-Car offers a seamless experience for renting vehicles of all types.",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
 			linkText: "View Project",
-			link: "https://github.com",
+			link: "https://github.com/j-vaz12/Rent-A-Car",
 		},
 
 		{
-			title: "Project 4",
+			title: "Game-Buddy",
 			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+				"Game Buddy is a cutting-edge web application designed for passionate gamers who love to keep their gaming experiences organized and personalized. With an extensive API that covers a vast library of games, Game Buddy is the perfect platform for gamers to curate their collections, track their progress, and keep their wishlist up-to-date with the latest and greatest titles.",
 			logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
 			linkText: "View Project",
-			link: "https://github.com",
+			link: "https://github.com/j-vaz12/Game-Buddy",
 		},
 
 	],
